74f43049da5b7522b3c9c0871567b0e0da2543ff
hsanderr <henriquesander27@gmail.com>
2023-01-26 07:50:18 -0300

    finish how-to-use page #36

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,5 +1,19 @@
 module.exports = fn => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next);
+        } catch(e) {
+            next();
+        }
     }
 }
+// module.exports = fn => {
+//     try {
+//         fn(req, res, next);
+//     } catch (e) {
+        
+//     }
+//     return (req, res, next) => {
+//         fn(req, res, next).catch(next);
+//     }
+// }
