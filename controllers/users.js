d76c1e79fc9ac07924a24b73df973b3269275006
Henrique Sander Lourenço <henrique.sander@i-sensi.io>
2023-01-29 11:14:44 -0300

    change dashgen email

--- controllers/users.js
+++ controllers/users.js
@@ -5,9 +5,9 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'Zoho',
     auth: {
-        user: 'henriquesander27@gmail.com',
+        user: 'nao.responda@dashgen.tech',
         pass: process.env.EMAIL_PWD
     }
 });
@@ -62,12 +62,14 @@ module.exports.sendPwdEmail = async (req, res, next) => {
         const randCode = randomString.generateString();
         bcrypt.hash(randCode, 10, async function (err, hash) {
             if (!err) {
-                await user.update({ randCodeHash: hash });
+                await User.updateOne({ username: email }, { randCodeHash: hash, randCodeTimeStamp: Math.floor(Date.now() / 1000) });
                 const mailOptions = {
-                    from: 'henriquesander27@gmail.com',
+                    from: 'nao.responda@dashgen.tech',
                     to: email,
                     subject: 'Recuperar senha',
-                    html: `<h1>Olá!</h1><p>Para recuperar sua senha do DashGen, utilize o seguinte código: <b>${randCode}</b></p>
+                    html: `<h1>Olá!</h1>
+                            <p>Para recuperar sua senha do DashGen, utilize o seguinte código:</p>
+                            <h2>${randCode}</h2>
                             <p>Att.</p><p>Equipe DashGen</p>`
                 };
                 transporter.sendMail(mailOptions, function (error, info) {
@@ -94,23 +96,31 @@ module.exports.changePwd = async (req, res, next) => {
         req.flash('error', 'Ops, algo deu errado. Por favor, tente recuperar sua senha novamente.');
         req.redirect('/forgotPwd');
     } else {
-        bcrypt.compare(code, user.randCodeHash, async function (error, result) {
-            if (!error) {
-                if (result) {
-                    user.setPassword(password, async function () {
-                        user.randCodeHash = undefined;
-                        await user.save();
-                        req.flash('success', 'Senha atualizada com sucesso.');
-                        res.redirect('/');
-                    });
+        if (Math.floor(Date.now() / 1000) > user.randCodeTimeStamp + 5 * 60) {
+            await User.updateOne({ username: email }, { randCodeHash: undefined, randCodeTimeStamp: undefined });
+            req.flash('error', 'Ops, parece que seu código expirou. Por favor, tente novamente.');
+            res.redirect('/forgotPwd');
+        }
+        else {
+            bcrypt.compare(code, user.randCodeHash, async function (error, result) {
+                if (!error) {
+                    if (result) {
+                        user.setPassword(password, async function () {
+                            user.randCodeHash = undefined;
+                            user.randCodeTimeStamp = undefined;
+                            await user.save();
+                            req.flash('success', 'Senha atualizada com sucesso.');
+                            res.redirect('/');
+                        });
+                    } else {
+                        const message = 'Código incorreto.'
+                        res.render('users/changePwd', { message, email });
+                    }
                 } else {
-                    const message = 'Código incorreto.'
-                    res.render('users/changePwd', { message, email });
+                    req.flash('error', 'Ops, algo deu errado. Por favor, tente novamente.');
+                    req.redirect('/forgotPwd');
                 }
-            } else {
-                req.flash('error', 'Ops, algo deu errado. Por favor, tente novamente.');
-                req.redirect('/forgotPwd');
-            }
-        });
+            });
+        }
     }
 }
